e8fda701e833e7a1e9015be8d61cab580522f1f1
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-05-17 20:12:04 +0200

    comptes, viteuf

--- coloc7/urls.py
+++ coloc7/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'coloc7.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^comptes/', include('comptes.urls', namespace='comptes')),
     url(r'^forum/', include('pybb.urls', namespace='pybb')),
     url(r'^admin/', include(admin.site.urls)),
 )
