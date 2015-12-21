67119623911d04940a8844c2f9f57de1c67f0449
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-21 12:52:35 +0100

    urls 1.8

--- coloc7/urls.py
+++ coloc7/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^comptes/', include('comptes.urls', namespace='comptes')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('lcl.urls', namespace='lcl')),
-)
+urlpatterns = [
+        url(r'^accounts/', include('django.contrib.auth.urls')),
+        url(r'^comptes/', include('comptes.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'', include('lcl.urls')),
+        ]
